93000a9213c04b6e4067fd64713b9e453de562a0
haim lavi <haimlavi1989@gmail.com>
2020-08-08 19:12:16 +0300

    refactoring routs step 3

--- app.js
+++ app.js
@@ -1,110 +1,12 @@
-const fs = require('fs');
 const express = require('express');
+3;
+const postsRoutes = require('./routes/postsRoutes');
 
 const app = express();
 
 // midllware - enable request body
 app.use(express.json());
 
-const port = 3000;
-postsDataPath = `${__dirname}/dev-data/data/posts.json`;
-const posts = JSON.parse(fs.readFileSync(postsDataPath));
-const postsRoutes = express.Router();
-
-const getAllPosts = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      posts,
-    },
-  });
-};
-
-const getPost = (req, res) => {
-  const id = req.params.id * 1;
-  if (id > posts.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
-  const post = posts.find((el) => {
-    return el.id === id;
-  });
-  res.status(200).json({
-    status: 'success',
-    data: {
-      post,
-    },
-  });
-};
-
-const addNewPost = (req, res) => {
-  NewId = posts[posts.length - 1].id + 1;
-  NewPost = Object.assign({ id: NewId }, req.body);
-  posts.push(NewPost);
-  fs.writeFile(postsDataPath, JSON.stringify(posts), (err) => {});
-  res.status(201).json({
-    status: 'success',
-    results: posts.length,
-    data: {
-      post: NewPost,
-    },
-  });
-};
-
-const updatePost = (req, res) => {
-  const id = req.params.id * 1;
-  if (id > posts.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
-
-  const index = posts.findIndex((el) => {
-    return el.id === id;
-  });
-
-  const post = posts[index];
-  const updatePost = Object.assign(post, req.body);
-  posts[index] = updatePost;
-  fs.writeFile(postsDataPath, JSON.stringify(posts), (err) => {});
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      post: updatePost,
-    },
-  });
-};
-
-const deletePost = (req, res) => {
-  const id = req.params.id * 1;
-  if (id > posts.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
-  const index = posts.findIndex((el) => {
-    return el.id === id;
-  });
-
-  posts.splice(index, 1);
-  fs.writeFile(postsDataPath, JSON.stringify(posts), (err) => {});
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-postsRoutes.route('/').get(getAllPosts).post(addNewPost);
-postsRoutes.route('/:id').get(getPost).patch(updatePost).delete(deletePost);
-
 app.use('/api/v1/posts', postsRoutes);
 
-app.listen(port, () => {
-  console.log(`running on port:${port}`);
-});
+module.exports = app;
