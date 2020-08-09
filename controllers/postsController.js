31bc10609563002c8b88fb9525e1c69912bd16a1
haim lavi <haimlavi1989@gmail.com>
2020-08-09 12:24:03 +0300

    refactoring routs step 4- split to controller

---
+++ controllers/postsController.js
@@ -0,0 +1,93 @@
+const fs = require('fs');
+
+dataPath = `${__dirname}/../dev-data/data/posts.json`;
+const posts = JSON.parse(fs.readFileSync(dataPath));
+
+exports.getAllPosts = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      posts,
+    },
+  });
+};
+
+exports.getPost = (req, res) => {
+  const id = req.params.id * 1;
+  if (id > posts.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+  const post = posts.find((el) => {
+    return el.id === id;
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      post,
+    },
+  });
+};
+
+exports.addNewPost = (req, res) => {
+  NewId = posts[posts.length - 1].id + 1;
+  NewPost = Object.assign({ id: NewId }, req.body);
+  posts.push(NewPost);
+  fs.writeFile(dataPath, JSON.stringify(posts), (err) => {});
+  res.status(201).json({
+    status: 'success',
+    results: posts.length,
+    data: {
+      post: NewPost,
+    },
+  });
+};
+
+exports.updatePost = (req, res) => {
+  const id = req.params.id * 1;
+  if (id > posts.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+
+  const index = posts.findIndex((el) => {
+    return el.id === id;
+  });
+
+  const post = posts[index];
+  const updatePost = Object.assign(post, req.body);
+  posts[index] = updatePost;
+  fs.writeFile(dataPath, JSON.stringify(posts), (err) => {});
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      post: updatePost,
+    },
+  });
+};
+
+exports.deletePost = (req, res) => {
+  const id = req.params.id * 1;
+  if (id > posts.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'invalid id',
+    });
+  }
+  const index = posts.findIndex((el) => {
+    return el.id === id;
+  });
+
+  posts.splice(index, 1);
+  fs.writeFile(dataPath, JSON.stringify(posts), (err) => {});
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
--- routes/postsRoutes.js
+++ routes/postsRoutes.js
@@ -1,100 +1,13 @@
 const express = require('express');
-const fs = require('fs');
 const Router = express.Router();
+const postsController = require('./../controllers/postsController');
 
-dataPath = `${__dirname}/../dev-data/data/posts.json`;
-const posts = JSON.parse(fs.readFileSync(dataPath));
-
-const getAllPosts = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      posts,
-    },
-  });
-};
-
-const getPost = (req, res) => {
-  const id = req.params.id * 1;
-  if (id > posts.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
-  const post = posts.find((el) => {
-    return el.id === id;
-  });
-  res.status(200).json({
-    status: 'success',
-    data: {
-      post,
-    },
-  });
-};
-
-const addNewPost = (req, res) => {
-  NewId = posts[posts.length - 1].id + 1;
-  NewPost = Object.assign({ id: NewId }, req.body);
-  posts.push(NewPost);
-  fs.writeFile(dataPath, JSON.stringify(posts), (err) => {});
-  res.status(201).json({
-    status: 'success',
-    results: posts.length,
-    data: {
-      post: NewPost,
-    },
-  });
-};
-
-const updatePost = (req, res) => {
-  const id = req.params.id * 1;
-  if (id > posts.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
-
-  const index = posts.findIndex((el) => {
-    return el.id === id;
-  });
-
-  const post = posts[index];
-  const updatePost = Object.assign(post, req.body);
-  posts[index] = updatePost;
-  fs.writeFile(dataPath, JSON.stringify(posts), (err) => {});
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      post: updatePost,
-    },
-  });
-};
-
-const deletePost = (req, res) => {
-  const id = req.params.id * 1;
-  if (id > posts.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'invalid id',
-    });
-  }
-  const index = posts.findIndex((el) => {
-    return el.id === id;
-  });
-
-  posts.splice(index, 1);
-  fs.writeFile(dataPath, JSON.stringify(posts), (err) => {});
-
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
-};
-
-Router.route('/').get(getAllPosts).post(addNewPost);
-Router.route('/:id').get(getPost).patch(updatePost).delete(deletePost);
+Router.route('/')
+  .get(postsController.getAllPosts)
+  .post(postsController.addNewPost);
+Router.route('/:id')
+  .get(postsController.getPost)
+  .patch(postsController.updatePost)
+  .delete(postsController.deletePost);
 
 module.exports = Router;
