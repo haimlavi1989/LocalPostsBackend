9d057979b620921e82aa550665b245185f7280fb
haim lavi <haimlavi1989@gmail.com>
2020-08-30 16:46:18 +0300

    Add global Error Handler

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const postsRoutes = require('./routes/postsRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
@@ -7,5 +9,11 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/posts', postsRoutes);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
