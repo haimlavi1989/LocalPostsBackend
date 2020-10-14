a1b2eea2f0ec3b03c239e6b6288d3bbc82fee1dd
haim lavi <haimlavi1989@gmail.com>
2020-10-14 12:43:51 +0300

    Fix some security issues

--- app.js
+++ app.js
@@ -3,12 +3,29 @@ const postsRoutes = require('./routes/postsRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
+// Start express app
 const app = express();
 
-// midllware - enable request body
+// Enable request body
 app.use(express.json());
 
+// ****** Security ******
+
+// Set security HTTP headers
+app.use(helmet());
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+// Preventing Parameter Pollution
+app.use(hpp());
+
+// Routes
 app.use('/api/v1/posts', postsRoutes);
 app.use('/api/v1/users', userRouter);
 
